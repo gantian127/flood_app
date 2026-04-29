51b6960eaf8af8e83b8db4e4bc40842d746fe37d
gantian127 <jamy127@foxmail.com>
2026-04-29 14:28:36 -0600

    add testing code for investment in evaluation

--- flood_app/evaluation.py
+++ flood_app/evaluation.py
@@ -259,8 +259,9 @@ class ModelEvaluation:
         investment = berm1_investment + berm2_investment + mulching_investment
 
         # !! Testing
-        # print(berm_cells, mulching_cells)
-        # print(berm_investment, mulching_investment)
+        # print(f"number of cells: berm 2m: {berm2_cells}, berm 1m：{berm1_cells}, mulching: {mulching_cells}")
+        # print(f"cost: berm 2m: {berm2_investment}, berm 1m {berm1_investment}, mulching: {mulching_investment}")
+        # print(f"total investment: {investment}")
 
         # TODO, only keep investment when efficiency function is implemented
         return investment, mulching_investment, berm1_investment, berm2_investment
